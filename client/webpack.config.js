125ccabe46310c610b947a5ad41b0e31f22e73c4
joe <jajohnson0201@gmail.com>
2023-03-23 14:53:51 -0500

    got src and webpack.config straightend out (file linking)

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,45 +20,46 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Webpack Plugin',
+        title: 'J.A.T.E',
       }),
-      new MiniCssExtractPlugin(),
       new InjectManifest({
         swSrc: '/src-sw.js',
-        swDest: 'service-worker.js',
+        swDest: 'src-sw.js',
       }),
-    //   new WebpackPwaManifest({
-    //     name: 'My Progressive Web App',
-    // short_name: 'MyPWA',
-    // description: 'My awesome Progressive Web App!',
-    // background_color: '#ffffff',
-    // crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
-    // icons: [
-    //   {
-    //   src: path.resolve('./src/assets/logo.png'),
-    //   sizes: [96, 128, 192, 256, 384, 512]
-    // }
-    // ]
-    //   }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      })
     ],
 
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, 'css-loader'],
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
+          use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
         },
