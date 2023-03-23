deee5b7ad3e46908916f249a196fda87c20e774f
joe <jajohnson0201@gmail.com>
2023-03-23 12:34:47 -0500

    set up event listeners for install

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,9 +23,22 @@ module.exports = () => {
         title: 'Webpack Plugin',
       }),
       new MiniCssExtractPlugin(),
-      new WebpackPwaManifest({
+      new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'My Progressive Web App',
+    short_name: 'MyPWA',
+    description: 'My awesome Progressive Web App!',
+    background_color: '#ffffff',
+    crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+    icons: [
+      {
+      src: path.resolve('./src/assets/logo.png'),
+      sizes: [96, 128, 192, 256, 384, 512]
+    }
+    ]
       }),
     ],
 
