164a89b1bc6d9d415e46ef55680b089a914157cd
joe <jajohnson0201@gmail.com>
2023-03-23 13:06:58 -0500

    added the db methods for storing indexeddb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,9 +14,9 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => { 
-  const db = await openDB('contact', 1);
-  const tx = db.transaction('contact', 'readwrite');
-  const store = tx.objectStore('contact');
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
   const request = store.put(content);
   const result = await request;
   console.log('putDb implemented',result);
@@ -25,9 +25,9 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const db = await openDB('contact', 1);
-  const tx = db.transaction('contact', 'readwrite');
-  const store = tx.objectStore('contact');
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
 console.error('getDb implemented', result)
